14885a38e7b8088821ec068de19cace457605170
moeen775 <89000833+moeen775@users.noreply.github.com>
2022-06-05 00:29:01 +0600

    Add files via upload

---
+++ bookreader.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import PyPDF2
+
+book = open('bookname.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+speaker = pyttsx3.init()
+for num in range(7,pages):
+    page = pdfReader.getPage(7)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
